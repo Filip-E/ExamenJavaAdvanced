ceb0ec2385f3648bb908ee46e72260cbfd0d137f
FilipElen <filipelengit@outlook.com>
2018-04-18 13:53:47 +0200

    vraag1: adds all functionality except GUI

--- ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/jpa/Question.java
+++ ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/jpa/Question.java
@@ -1,7 +1,43 @@
 package edu.ap.spring.jpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Question {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    String question;
+
+    @Column
+    String answer;
+
+    public Question() {
+    }
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
--- ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
+++ ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
@@ -1,9 +1,19 @@
 package edu.ap.spring.model;
+import edu.ap.spring.jpa.Question;
+import edu.ap.spring.jpa.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Component
 public class EightBall {
-	
+
+	private Random gen;
+	private int[] randomNumberArray;
+    @Autowired
+    private QuestionRepository repository;
+
 	private String[] answers = {"It is certain", 
 								"Yes definitely", 
 								"Most likely",
@@ -12,14 +22,40 @@ public class EightBall {
 								"Cannot predict now",
 								"Don't count on it", 
 								"Outlook not so good"};
-	
-	public String getRandomAnswer(String question) {
-		String answer = "";
 
-		return answer;
+	private int counter;
+	public String getRandomAnswer(String question) {
+        Question questionInDb = repository.findByQuestion(question);
+        String answer = "";
+        if (questionInDb != null) {
+            if(questionInDb.getQuestion() == question) {
+                return questionInDb.getAnswer();
+            }
+        }else{
+            answer = answers[randomNumberArray[counter]];
+            counter++;
+            if(counter > 9){
+                counter = 0;
+                fillRandomNumberArray();
+            }
+        }
+        return answer;
 	}
 
-	public String[] getAnswers() {
+    public EightBall() {
+	    gen = new Random();
+        randomNumberArray = new int[answers.length];
+        fillRandomNumberArray();
+        counter = 0;
+    }
+
+    public void fillRandomNumberArray(){
+        for (int i = 0; i < randomNumberArray.length; i++) {
+            randomNumberArray[i] = gen.nextInt(10);
+        }
+    }
+
+    public String[] getAnswers() {
 		return answers;
 	}
 
